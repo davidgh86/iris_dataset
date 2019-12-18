12f3a33f9aab429f590ea1e95828a6d2a9441bdf
David <david_gh86@hotmail.es>
2019-12-18 19:52:48 +0100

    complete linear svc

--- iris_dataset_svc.py
+++ iris_dataset_svc.py
@@ -1,5 +1,3 @@
-import numpy as np
-import matplotlib.pyplot as plt
 from matplotlib import style
 from sklearn import svm, datasets
 from sklearn.model_selection import train_test_split
@@ -14,6 +12,13 @@ y = iris_dataset.target
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, shuffle=True)
 
 clf = svm.SVC(kernel='linear', C=1.0)
-clf.fit(X, y)
+clf.fit(X_train, y_train)
+print(clf.coef_)
+print(clf.score(X, y))
+print(clf.score(X_test, y_test))
+
+#for i in range(len(iris_dataset.data)):
+#    print(clf.predict([iris_dataset.data[i]]), iris_dataset.target[i])
+#print(clf.predict([[5.4, 3.9, 1.7, 0.4]))
 
 
